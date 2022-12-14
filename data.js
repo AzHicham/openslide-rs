cbe3381e8e74ce8ea600b895f823aa336f488b71
github-action-benchmark <github@users.noreply.github.com>
2022-12-14 13:32:47 +0000

    add openslide-rs Benchmark (cargo) benchmark result for 1478d34ed8138c5ff7962b3028e1430fc9b3844c

--- data.js
+++ data.js
@@ -1,68 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671024385551,
+  "lastUpdate": 1671024767615,
   "repoUrl": "https://github.com/AzHicham/openslide-rs",
   "entries": {
     "openslide-rs Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "79a616858b6bf8cd766edf58824c58524dba9fa0",
-          "message": "build(deps): bump MeilCli/regex-match from 1.5.15 to 1.5.16 (#16)\n\nBumps [MeilCli/regex-match](https://github.com/MeilCli/regex-match) from 1.5.15 to 1.5.16.\r\n- [Release notes](https://github.com/MeilCli/regex-match/releases)\r\n- [Commits](https://github.com/MeilCli/regex-match/compare/v1.5.15...v1.5.16)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: MeilCli/regex-match\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2022-11-01T11:51:57+01:00",
-          "tree_id": "fec282463167694f0047c2a8ac44d7826f392bc2",
-          "url": "https://github.com/AzHicham/openslide-rs/commit/79a616858b6bf8cd766edf58824c58524dba9fa0"
-        },
-        "date": 1667300161775,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "deepzoom_read_image_256",
-            "value": 2112877,
-            "range": "± 16720",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "deepzoom_read_image_512",
-            "value": 8599450,
-            "range": "± 73138",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "openslide_read_image_256",
-            "value": 2035245,
-            "range": "± 12128",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "openslide_read_image_512",
-            "value": 8627207,
-            "range": "± 115929",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "openslide_read_region_256",
-            "value": 1957698,
-            "range": "± 11756",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "openslide_read_region_512",
-            "value": 8282700,
-            "range": "± 55732",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -1715,6 +1655,66 @@ window.BENCHMARK_DATA = {
             "name": "openslide_read_region_512",
             "value": 9557306,
             "range": "± 1783025",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hicham.azimani@owkin.com",
+            "name": "HichamAz",
+            "username": "AzHicham"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1478d34ed8138c5ff7962b3028e1430fc9b3844c",
+          "message": "refactor(deepzoom): reduce usage of for-loop & mut (#29)",
+          "timestamp": "2022-12-14T14:29:06+01:00",
+          "tree_id": "905734614b821cfdd5ee705431757eff276d2a41",
+          "url": "https://github.com/AzHicham/openslide-rs/commit/1478d34ed8138c5ff7962b3028e1430fc9b3844c"
+        },
+        "date": 1671024766500,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "deepzoom_read_image_256",
+            "value": 2412082,
+            "range": "± 436630",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "deepzoom_read_image_512",
+            "value": 9616407,
+            "range": "± 1288906",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "openslide_read_image_256",
+            "value": 2252665,
+            "range": "± 254576",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "openslide_read_image_512",
+            "value": 9464080,
+            "range": "± 1327696",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "openslide_read_region_256",
+            "value": 2124185,
+            "range": "± 291107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "openslide_read_region_512",
+            "value": 9481671,
+            "range": "± 1923146",
             "unit": "ns/iter"
           }
         ]
